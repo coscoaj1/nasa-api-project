44c99e1df14d0570759835dff6ef54859a9740e9
coscoaj1 <coscoanthony1@gmail.com>
2022-01-08 10:59:14 -0500

    header

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <nav className="w-full flex h-12 sm:h-24 gap-4 bg-black font-sans">
-      <ul className="flex text-white">
+    <nav className="relative w-full flex h-12 sm:h-16 gap-4 bg-black font-sans">
+      <ul className="flex text-white items-center pl-6">
         <li>NASA APOD: Astronomy Picture of the Day</li>
       </ul>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,7 @@ import "../styles/globals.css";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
