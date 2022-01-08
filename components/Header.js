6e5e2e602f71bef5226ce382eccdf03525083e27
coscoaj1 <coscoanthony1@gmail.com>
2022-01-08 11:35:36 -0500

    Header

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <nav className="relative w-full flex h-12 sm:h-16 gap-4 bg-black font-sans">
+    <nav className="relative w-full flex h-12 gap-4 bg-black font-sans">
       <ul className="flex text-white items-center pl-6">
-        <li>NASA APOD: Astronomy Picture of the Day</li>
+        <li>UnSpace</li>
       </ul>
     </nav>
   );
