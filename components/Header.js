9b0b2204ddbe0f8f48d9838b60de8d4bffa2946b
coscoaj1 <coscoanthony1@gmail.com>
2022-01-09 13:55:19 -0500

    added tests

--- components/Header.js
+++ components/Header.js
@@ -5,16 +5,14 @@ import { HiSun } from "react-icons/hi";
 export default function Header({ theme, toggleTheme }) {
   return (
     <nav className="relative flex w-full h-12 gap-4 dark:bg-[#212121] bg-white">
-      <ul className="flex items-center pl-6">
-        <li className="text-lg font-[500]">UnSpace</li>
-        <button className="p-2 rounded-full" onClick={toggleTheme}>
-          {theme === "dark" ? (
-            <HiSun className="w-8 h-8" />
-          ) : (
-            <HiMoon className="w-8 h-8" />
-          )}
-        </button>
-      </ul>
+      <h1 className="text-lg font-[500]">UnSpace</h1>
+      <button className="p-2 rounded-full" onClick={toggleTheme}>
+        {theme === "dark" ? (
+          <HiSun className="w-8 h-8" />
+        ) : (
+          <HiMoon className="w-8 h-8" />
+        )}
+      </button>
     </nav>
   );
 }
